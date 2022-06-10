3425b41f468251cd50bc22d540f7a9a4028bb253
matheuscaso <matheuscasoares@gmail.com>
2022-06-10 15:13:09 -0300

    commit inicial

---
+++ src/main/java/com/projects/studiohair/StudioHairApplication.java
@@ -0,0 +1,13 @@
+package com.projects.studiohair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioHairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioHairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/studiohair/StudioHairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.studiohair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioHairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
